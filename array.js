a6bdcc6833ac32a98c96e6400b9a88a2f2319282
akash rattan <akashrattan43@Gmail.com>
2020-11-14 21:06:36 -0800

    mapping

--- array.js
+++ array.js
@@ -1,10 +1,31 @@
-const name = ['Ak', 'drake']
+// const name = ['Ak', 'drake']
 
-name.push('Rattan')
+// name.push('Rattan')
 
-console.log(name)
+// // console.log(name)
 
-let bank = 250
+// let bank = 250
 
-bank = 100
-console.log(bank)
+// bank = 100
+// // console.log(bank)
+
+// function repeater(fn) {
+//     fn()
+//     fn()
+//     fn()
+// }
+
+// function sayHello() {
+//     console.log('hello')
+// }
+
+// repeater(sayHello)
+
+const video = ["pranking someboy", "how to java", "html tutorial"];
+
+//map
+const newVideos = video.map(function (video) {
+  return video.toUpperCase();
+});
+
+console.log(newVideos);
